1767359a5c029e5607efd0d2585e80e83392d1ff
Buggem <66670928+Buggem@users.noreply.github.com>
2022-07-20 19:14:23 +1000

    Update cuttlefish.js

--- renderers/cuttlefish.js
+++ renderers/cuttlefish.js
@@ -3,23 +3,6 @@ const currentDate = new Date();
 var version = 0.1;
 console.log("Pixelmapper for P5.js (codename Cuttlefish) v" + version.toString() + " renderer OFFICIAL\n\u00A9 The Pixelmap Authors " + currentDate.getFullYear()); // Making my life easier so I never have to update the year.
 var pxname = "Official Pixelmapper for P5.js (codename Cuttlefish) v" + version.toString() + " by The Pixelmap Authors";
-function unofficialFound() {
-	var renderID = Math.floor(Math.random() * 1000).toString();
-	if(window.localStorage.hasOwnProperty(renderID)) {
-		this();
-		return;
-	}
-	console.warn("WARNING: Unofficial renderer found using same variable name as official.\nIf you are the developer of this renderer please use a different variable name.\nThis render will now be sent to localstorage as render #" + renderID);
-	window.localStorage.setItem(renderID, JSON.stringify(window.pixelmap));
-	window.pixelmap = { "name": pxname };
-}
-if (typeof window.pixelmap === 'undefined') {
-	// This case create a new one
-	window.pixelmap = { "name": pxname };
-} else if(window.pixelmap.name !== pxname) {
-	// This case warn the user
-	unofficialFound();
-}
 var pmp5 = {
 	"render": function(pm, pos, sketch) {
 		var renderPixel = function(pixel, pixelsize, pos, sketch) {
@@ -40,4 +23,21 @@ var pmp5 = {
 			sketch.rect(pos.x + (pixel.x*pixelsize), pos.y + (pixel.y*pixelsize), pixelsize, pixelsize);
 	}
 };
-window.pixelmap.p5 = pmp5;
+function unofficialFound() {
+	var renderID = Math.floor(Math.random() * 1000).toString();
+	if(window.localStorage.hasOwnProperty(renderID)) {
+		this();
+		return;
+	}
+	console.warn("WARNING: Unofficial renderer found using same variable name as official.\nIf you are the developer of this renderer please use a different variable name.\nThis render will now be sent to localstorage as render #" + renderID);
+	window.localStorage.setItem(renderID, JSON.stringify(window.pixelmap));
+	window.pixelmap = pmp5;
+}
+if (typeof window.pixelmap === 'undefined') {
+	// This case create a new one
+	window.pixelmap = pmp5;
+} else if(window.pixelmap.name !== pxname) {
+	// This case warn the user
+	unofficialFound();
+}
+//window.pixelmap.p5 = pmp5;
