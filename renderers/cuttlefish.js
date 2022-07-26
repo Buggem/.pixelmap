ad83ceb6d6c943770cd2772df07ecf20692216f0
dev01111 <105835627+dev01111@users.noreply.github.com>
2022-07-26 18:03:29 +1000

    Nipper + more comments and bug fixes
    Hopefully we'll have more documentation in future situations.

--- renderers/cuttlefish.js
+++ renderers/cuttlefish.js
@@ -1,12 +1,12 @@
 //window.pixelmap = { "name": "COOLERRENDER" }; // Unofficial render test
-const currentDate = new Date();
-var version = 0.1;
-console.log("Pixelmapper for P5.js (codename Cuttlefish) v" + version.toString() + " renderer OFFICIAL\n\u00A9 The Pixelmap Authors " + currentDate.getFullYear()); // Making my life easier so I never have to update the year.
-var pxname = "Official Pixelmapper for P5.js (codename Cuttlefish) v" + version.toString() + " by The Pixelmap Authors";
+const currentDate = new Date(); // So I never have to change the date
+var version = 0.1; // Version represented as decimal int
+console.log("Pixelmapper (aka NIPPER) for P5.js (codename Cuttlefish) v" + version.toString() + " renderer OFFICIAL\n\u00A9 The Pixelmap Authors " + currentDate.getFullYear()); // Making my life easier so I never have to update the year.
+var pxname = "Official Pixelmapper (aka NIPPER) for P5.js (codename Cuttlefish) v" + version.toString() + " by The Pixelmap Authors";
 var pmp5 = {
-	"logs": false,
-	"render": function(pm, pos, sketch) {
-		var renderPixel = function(pixel, pixelsize, pos, sketch) {
+	"logs": false, // Do logs? boolean
+	"render": function(pm, pos, sketch) { // Main render function
+		var renderPixel = function(pixel, pixelsize, pos, sketch) { // Alias of pmp5.renderPixel()
 			if(window.pixelmap.p5.logs) console.log("Rendered pixel at x: " + pos.x + (pixel.x*pixelsize) + ", y: " + pos.y + (pixel.y*pixelsize));
 			//console.log(sketch);
 			sketch.noStroke();
@@ -18,14 +18,14 @@ var pmp5 = {
 		}
 		if(window.pixelmap.p5.logs) console.log("Finished render of \"" + ((typeof pm.name === 'undefined') ? ("Unnamed Pixelmap") : (pm.name)) + "\" at x: " + pos.x + ", y: " + pos.y);
 	},
-	"renderPixel": function(pixel, pixelsize, pos, sketch) {
+	"renderPixel": function(pixel, pixelsize, pos, sketch) { // Renders a single pixel
 			if(window.pixelmap.p5.logs) console.log("Rendered pixel at x: " + pos.x + (pixel.x*pixelsize) + ", y: " + pos.y + (pixel.y*pixelsize));
 			//console.log(sketch);
 			sketch.noStroke();
 			sketch.fill(pixel.r, pixel.g, pixel.b, pixel.a);
 			sketch.rect(pos.x + (pixel.x*pixelsize), pos.y + (pixel.y*pixelsize), pixelsize, pixelsize);
 	},
-	"loadPixelmap": function(url, _callback) {
+	"loadPixelmap": function(url, _callback) { // Newly added load feature
 		  var xhttp = new XMLHttpRequest();
 		  xhttp.onreadystatechange = function() {
 		    if (this.readyState == 4 && this.status == 200) {
@@ -37,7 +37,7 @@ var pmp5 = {
 		  xhttp.open("GET", url, true);
 		  xhttp.send();
 	},
-	"genPixelmap": function(_name ,_pxsize, image, _callback) {
+	"genPixelmap": function(_name ,_pxsize, image, _callback) { // Even newer generator from a p5.Image!
 		var pm = { 
 			"name": _name,
 			"pixelsize": _pxsize,
@@ -54,7 +54,7 @@ var pmp5 = {
 		_callback(pm);
 	}
 };
-window.savePixelmap = function(pm) {
+window.savePixelmap = function(pm) { // Saves to LocalStorage
 	var pmID = Math.floor(Math.random() * 1000).toString();
 	if(window.localStorage.hasOwnProperty(pmID)) {
 		this();
@@ -62,24 +62,24 @@ window.savePixelmap = function(pm) {
 	}
 	if(window.pixelmap.p5.logs) console.log("Saving as Pixelmap #" + pmID);
 	window.localStorage.setItem(pmID, JSON.stringify(pm));
-};
-function unofficialFound() {
+}; // Hardly used saver
+function unofficialFound() { // Don't overwrite OUR variable
 	var renderID = Math.floor(Math.random() * 1000).toString();
-	if(window.localStorage.hasOwnProperty(renderID)) {
+	if(window.localStorage.hasOwnProperty(renderID)) { // This could get stuck in an infinite loop, clear all localstorage when fix needed
 		this();
 		return;
 	}
 	console.warn("WARNING: Unofficial renderer found using same variable name as official.\nIf you are the developer of this renderer please use a different variable name.\nThis render will now be sent to localstorage as render #" + renderID);
-	window.localStorage.setItem(renderID, JSON.stringify(window.pixelmap));
-	window.pixelmap = {};
-	window.pixelmap.p5 = pmp5;
+	window.localStorage.setItem(renderID, JSON.stringify(window.pixelmap)); // Bye bye other renderer, go into localstorage
+	window.pixelmap = { "name": pxname }; // important to write render.name otherwise line 81 window.pixelmap.name will always be undefined
+	window.pixelmap.p5 = pmp5; // DON'T REMOVE THIS LINE | DEFINES PIXELMAP.P5
 }
 if (typeof window.pixelmap === 'undefined') {
 	// This case create a new one
-	window.pixelmap = {};
-	window.pixelmap.p5 = pmp5;
+	window.pixelmap = { "name": pxname }; // important to write render.name otherwise line 81 window.pixelmap.name will always be undefined
+	window.pixelmap.p5 = pmp5; // DON'T REMOVE THIS LINE | DEFINES PIXELMAP.P5
 } else if(window.pixelmap.name !== pxname) {
 	// This case warn the user
-	unofficialFound();
+	unofficialFound(); // We found an unofficial 'reserved variable overwrite'
 }
 //window.pixelmap.p5 = pmp5;
