4e747c5def9391a1413c1bc193b4ab1e8ba3591a
Buggem <66670928+Buggem@users.noreply.github.com>
2022-07-29 16:02:59 +1000

    Create webhook.sh

---
+++ .github/webhook.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# Get branch and hash
+git_hash=$(git rev-parse --short "$GITHUB_SHA")
+git_branch=${GITHUB_REF#refs/heads/}
+
+echo "Commit ${hash} on ${git_branch}"
+
+curl -H 'Content-Type: application/json' -X POST "$DISCORDWEBHOOK" -d@- << EOF
+{
+    "username": "RepoReport",
+    "avatar_url": "https://i.stack.imgur.com/5dVWT.jpg",
+    "content": "Commit ${hash} on ${git_branch}"
+}
+EOF
