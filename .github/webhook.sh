15e02b49cc74fd77d6f644a9e845f985895bd5be
Buggem <66670928+Buggem@users.noreply.github.com>
2022-08-19 17:48:52 +1000

    better neat than sheet

--- .github/webhook.sh
+++ .github/webhook.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Get branch and hash
-git_hash=$(git rev-parse --short "$GITHUB_SHA")
+git_hash=$(git rev-parse --short "${GITHUB_SHA}")
 git_branch=${GITHUB_REF#refs/heads/}
 
 echo "Commit ${git_hash} on ${git_branch}"
